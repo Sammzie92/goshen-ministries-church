7d48ba6d163996aaefc1d4d121f63ff619ae28c1
Sammzie92 <122966796+Sammzie92@users.noreply.github.com>
2023-04-17 13:42:22 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,128 @@
+/* Goshen ministries - Main JS*/
+
+(function($) {
+
+    "use strict";
+    
+    const cfg = {
+                scrollDuration : 800, // smoothscroll duration
+                mailChimpURL   : ''   // mailchimp url
+                };
+    const $WIN = $(window);
+
+    // Add the User Agent to the <html>
+
+    const doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
+
+
+    <!--Preloader script-->
+
+    window.addEventListener('load', function () {
+      setTimeout(function () {
+        document.getElementById('preloader').style.display = 'none';
+
+      }, 1500);
+    });
+
+ <!--Preloader script-->
+
+
+   /* Mobile Menu */
+    const ssMobileMenu = function() {
+
+        const toggleButton = $('.header-menu-toggle');
+        const nav = $('.header-nav-wrap');
+
+        toggleButton.on('click', function(event){
+            event.preventDefault();
+
+            toggleButton.toggleClass('is-clicked');
+            nav.slideToggle();
+        });
+
+        if (toggleButton.is(':visible')) nav.addClass('mobile');
+
+        $WIN.on('resize', function() {
+            if (toggleButton.is(':visible')) nav.addClass('mobile');
+            else nav.removeClass('mobile');
+        });
+
+        nav.find('a').on("click", function() {
+
+            if (nav.hasClass('mobile')) {
+                toggleButton.toggleClass('is-clicked');
+                nav.slideToggle(); 
+            }
+        });
+
+    };
+
+
+   /* Alert Boxes*/
+    const ssAlertBoxes = function() {
+
+        $('.alert-box').on('click', '.alert-box__close', function() {
+            $(this).parent().fadeOut(500);
+        }); 
+
+    };
+
+    
+   /* Smooth Scrolling */
+    const ssSmoothScroll = function() {
+        
+        $('.smoothscroll').on('click', function (e) {
+            const target = this.hash;
+            const $target = $(target);
+            
+            e.preventDefault();
+            e.stopPropagation();
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, cfg.scrollDuration, 'swing').promise().done(function () {
+
+                // check if menu is open
+                if ($('body').hasClass('menu-is-open')) {
+                    $('.header-menu-toggle').trigger('click');
+                }
+
+                window.location.hash = target;
+            });
+        });
+
+    };
+
+
+   /* Back to Top*/
+    const ssBackToTop = function() {
+        
+        const pxShow      = 500;
+        const $goTopButton = $(".ss-go-top")
+
+        // Show or hide the button
+        if ($(window).scrollTop() >= pxShow) $goTopButton.addClass('link-is-visible');
+
+        $(window).on('scroll', function() {
+            if ($(window).scrollTop() >= pxShow) {
+                if(!$goTopButton.hasClass('link-is-visible')) $goTopButton.addClass('link-is-visible')
+            } else {
+                $goTopButton.removeClass('link-is-visible')
+            }
+        });
+    };
+
+
+   /* Initialize */
+    (function ssInit() {
+
+
+        ssMobileMenu();
+        ssAlertBoxes();
+        ssSmoothScroll();
+        ssBackToTop();
+
+    })();
+
+})(jQuery);
